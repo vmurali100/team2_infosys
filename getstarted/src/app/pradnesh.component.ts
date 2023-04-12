89f13f9662283c622e95e99561c4898348ece0eb
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-04-12 17:58:43 +0530

    own module, component creation

---
+++ getstarted/src/app/pradnesh.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pradnesh',
+  template: "<h1>Hello! I'm Pradnesh Khedekar</h1>",
+  styles: ['h1 { background: black; color:white }'],
+})
+export class pradneshComponent {}
---
+++ getstarted/src/app/pradnesh.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { pradneshComponent } from './pradnesh.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [pradneshComponent],
+  imports: [BrowserModule],
+  exports: [],
+  bootstrap: [pradneshComponent],
+})
+export class pradneshModule {}
