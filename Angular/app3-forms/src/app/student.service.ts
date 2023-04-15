39b44fad1c75144f71879227ef7d9539dda4a7b3
vmurali100 <vmurali100@users.noreply.github.com>
2023-04-15 17:27:03 +0530

    Adding All API

--- Angular/app3-forms/src/app/student.service.ts
+++ Angular/app3-forms/src/app/student.service.ts
@@ -1,18 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 import { Student } from './student/student.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentService {
+  constructor(private _http: HttpClient) {}
 
-  constructor(private _http:HttpClient) { }
-
-  getAllStudents(){}
-  createStudent(student:Student){
-    return this._http.post("http://localhost:3000/students",student)
+  getAllStudents() {
+    return this._http.get('http://localhost:3000/students');
   }
-  updateStudent(){}
-  deleteStudent(){}
+  createStudent(student: Student) {
+    return this._http.post('http://localhost:3000/students', student)
+    
+    // .pipe(catchError(this.errorHandler))
+  }
+  updateStudent(student:Student) {
+    return this._http.put('http://localhost:3000/students/' + student.id,student)
+  }
+  deleteStudent(student: Student) {
+    return this._http.delete('http://localhost:3000/students/' + student.id);
+  }
+//   errorHandler(error: HttpErrorResponse) {
+//     return throwError(error.message || "server error.");
+// }
 }
