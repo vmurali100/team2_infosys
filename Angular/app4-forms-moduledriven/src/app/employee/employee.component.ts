91097c227768b7e0cba159e86a2f324f2b830f95
nisreen991 <50657375+nisreen991@users.noreply.github.com>
2023-04-17 16:04:50 +0530

    form validations

--- Angular/app4-forms-moduledriven/src/app/employee/employee.component.ts
+++ Angular/app4-forms-moduledriven/src/app/employee/employee.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 export interface Employee {
   fname: string;
   lname: string;
@@ -15,9 +15,9 @@ export class EmployeeComponent {
   employees: Employee[] = [];
   constructor(private formBuilder: FormBuilder) {
     this.employeeDetails = this.formBuilder.group({
-      fname: '',
-      lname: '',
-      email: '',
+      fname: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      lname: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
     });
   }
   addEmployee() {
