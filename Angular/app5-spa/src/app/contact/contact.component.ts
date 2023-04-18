7f24d6e8ced24b414d5b93af08a34b870d1ce4aa
nisreen991 <50657375+nisreen991@users.noreply.github.com>
2023-04-18 15:28:37 +0530

    minor changes

--- Angular/app5-spa/src/app/contact/contact.component.ts
+++ Angular/app5-spa/src/app/contact/contact.component.ts
@@ -9,23 +9,23 @@ import { Contact, ContactService } from '../contact.service';
 })
 export class ContactComponent {
   contactDetils: FormGroup;
-  contacts:Contact[] = [];
-  isEdit:boolean = false;
-  constructor(private fromBuilder: FormBuilder,private cs:ContactService) {
+  contacts: Contact[] = [];
+  isEdit: boolean = false;
+  constructor(private fromBuilder: FormBuilder, private cs: ContactService) {
     this.contactDetils = this.fromBuilder.group({
       fname: new FormControl('', []),
       lname: new FormControl('', []),
       email: new FormControl('', []),
       password: new FormControl('', []),
-      id:new FormControl(null,[])
+      id: new FormControl(null, [])
     });
   }
 
   addContact() {
-    console.log('addContact called !!',this.contactDetils.value);
-    this.cs.createContact(this.contactDetils.value).subscribe((res)=>{
+    console.log('addContact called !!', this.contactDetils.value);
+    this.cs.createContact(this.contactDetils.value).subscribe((res) => {
       console.log(res)
-      this.cs.getAllContacts().subscribe((response:any)=>{
+      this.cs.getAllContacts().subscribe((response: any) => {
         this.contacts = response
         this.clearForm()
 
@@ -33,29 +33,29 @@ export class ContactComponent {
     })
   }
 
-  clearForm(){
+  clearForm() {
     this.contactDetils.reset()
   }
-getAllUsers(){
-  this.cs.getAllContacts().subscribe((response:any)=>{
-    this.contacts = response
-  })
-}
-  deleteUserInfo(contact:any){
-    this.cs.deleteContact(contact).subscribe(()=>{
+  getAllUsers() {
+    this.cs.getAllContacts().subscribe((response: any) => {
+      this.contacts = response
+    })
+  }
+  deleteUserInfo(contact: any) {
+    this.cs.deleteContact(contact).subscribe(() => {
       this.getAllUsers()
     })
   }
 
-  editContact(contact:any){
+  editContact(contact: any) {
     this.contactDetils.setValue(contact)
     this.isEdit = !this.isEdit
   }
-  ngOnInit(){
+  ngOnInit() {
     this.getAllUsers()
   }
-  updateContact(){
-    this.cs.updateContact(this.contactDetils.value).subscribe(response=>{
+  updateContact() {
+    this.cs.updateContact(this.contactDetils.value).subscribe(response => {
       this.getAllUsers();
       this.clearForm();
       this.isEdit = !this.isEdit
