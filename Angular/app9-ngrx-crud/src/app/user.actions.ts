75fbe22a2272e1d1c4c1d46a83701fd33e723da0
vmurali100 <vmurali100@users.noreply.github.com>
2023-04-20 17:11:47 +0530

    CRUD Operation for Ngrx App

--- Angular/app9-ngrx-crud/src/app/user.actions.ts
+++ Angular/app9-ngrx-crud/src/app/user.actions.ts
@@ -5,6 +5,8 @@ export const UserApiActions = createActionGroup({
   source: 'Users API',
   events: {
     'Retrieve Users List': props<{ users: ReadonlyArray<User> }>(),
-    'Updte Users List': props<{ user: User }>(),
+    'Delete Users List': props<{ user: User }>(),
+    'Add Users List': props<{ user: User }>(),
+    'Update Users List': props<{ user: User }>(),
   },
 });
